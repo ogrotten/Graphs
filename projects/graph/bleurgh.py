311393b12417a662b31c2621f39597172e541961
chip <chiposaur@gmail.com>
2020-04-13 16:07:12 -0500

    .

---
+++ projects/graph/bleurgh.py
@@ -0,0 +1,7 @@
+thing = ['Waltz', 'Tango', 'Viennese Waltz', 'Foxtrot', 'Cha Cha', 'Samba', 'Rumba', 'Paso Doble', 'Jive']
+
+newthing = sorted(thing)
+
+for thing in newthing:
+	print(f"{thing}")
+
